1e269c16eef5aac74ab095760b814a1a9c18ee77
Saulo de Tarso <de_tarso7@hotmail.com>
2023-06-11 18:15:36 -0300

    Criando validações do usuário

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,6 +2,42 @@ const Users = require('../models/Users')
 
 module.exports = class UserController{
     static async register (req, res){
-        res.json('Rota de registro')
+        
+        const {name, email, phone, password, confirmpassword} = req.body
+
+        if(!name){
+            res.status(422).json({massage: 'O nome é obrigatório'})
+            return
+        }
+        if(!email){
+            res.status(422).json({massage: 'O email é obrigatório'})
+            return
+        }
+        if(!phone){
+            res.status(422).json({massage: 'O telefone é obrigatório'})
+            return
+        }
+        if(!password){
+            res.status(422).json({massage: 'A senha é obrigatório'})
+            return
+        }
+        if(!confirmpassword){
+            res.status(422).json({massage: 'Repita a senha'})
+            return
+        }
+
+        if(password !== confirmpassword){
+            res.status(422).json({massage: 'As senhas não conferem'})
+            return
+        }
+
+        //Check if user exist
+
+        const checkUser = User.findOnde({email: email})
+
+        if (checkUser){
+            res.status(422).json({messagem: 'O email já está cadastrado. Utilize outro email'})
+        }
+
     }
 }
